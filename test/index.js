10750a60c853fcca91967e7df5a34407fef37ae3
钟爱东 <zhongaidong@umpay.com>
2023-09-12 11:12:00 +0800

    【修改内容】 :  1 【reviewer】  :  钟爱东

---
+++ test/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author       : zhongaidong
+ * @Date         : 2023-09-12 11:10:41
+ * @Description  :
+ */
+window.onload = function() {
+  document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+}
